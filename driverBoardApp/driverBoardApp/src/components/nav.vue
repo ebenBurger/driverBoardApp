4398518155be6a3487ff651f3024344cf8d1e8a6
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-22 13:21:04 +0200

    nav created

--- driverBoardApp/driverBoardApp/src/components/nav.vue
+++ driverBoardApp/driverBoardApp/src/components/nav.vue
@@ -1,24 +1,21 @@
 <template>
     <div>
-        <div v-for="(option, index) in options" :model="selected" :key="index">
-            <p>{{option.title}}</p>
-            <p><font-awesome-icon icon='option.icon' /></p>
-        </div>
+        <b-nav horizontal>
+            <b-list-group-item>
+                <router-link to="/">
+                    <b-icon-house-door></b-icon-house-door>Home
+                </router-link>
+                <router-link to="/about">
+                    <b-icon-house-door></b-icon-house-door>About
+                </router-link>
+            </b-list-group-item>
+        </b-nav>
     </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        selected: 1,
-        options: [
-            { id: 1, icon: "Home", title: "Home", color: 'black'},
-            { id: 2, icon: "LayerGroup", title: "Category" },
-            { id: 3, icon: "Cog", title: "Setting" },
-            { id: 4, icon: "User", title: "Account", badge: 15 },
-            { id: 5, icon: "Bell", title: "Notification" },
-        ],
-    }),
+    data: () => ({}),
     beforeCreate() {
     },
     created() {
