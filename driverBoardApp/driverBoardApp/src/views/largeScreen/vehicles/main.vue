340c09262743cdebb57c31c1a52b964534e611b3
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-25 09:05:11 +0200

    vehicle view updated

--- driverBoardApp/driverBoardApp/src/views/largeScreen/vehicles/main.vue
+++ driverBoardApp/driverBoardApp/src/views/largeScreen/vehicles/main.vue
@@ -16,46 +16,40 @@
                     
                     <b-row class="mt-3">
                         <b-col>
-                            <b-card>
-                                <b-row>
-                                    <b-col>
-                                        <B-table
-                                            striped hover
-                                            :items="vehicleData.dataSource"
-                                            :fields="vehicleData.tableColumns"
-                                            :busy="vehicleData.isLoading"
-                                            @row-clicked="openVehicle"
-                                            id="vehicleTable"
-                                            :current-page="vehicleData.currentPage">
+                            <B-table
+                                striped hover
+                                :items="vehicleData.dataSource"
+                                :fields="vehicleData.tableColumns"
+                                :busy="vehicleData.isLoading"
+                                @row-clicked="openVehicle"
+                                id="vehicleTable"
+                                :current-page="vehicleData.currentPage">
 
-                                            <template #table-busy>
-                                                <div class="text-center my-2">
-                                                    <b-spinner style="width: 3rem; height: 3rem;"></b-spinner>
-                                                </div>
-                                            </template>
-                                            
-                                            <template #cell(actions)="row">
-                                                <b-row align-v="center" align-h="end">
-                                                    <b-button @click="openVehicle(row.item)" size="sm" class="btn-icon">
-                                                        <b-icon-chevron-right></b-icon-chevron-right>
-                                                    </b-button>
-                                                </b-row>
-                                            </template>
-                                            
-                                        </B-table>
-                                    </b-col>
-                                </b-row>
-                                
-                                <b-row align-h="center" >
-                                    <b-pagination
-                                        v-model="vehicleData.currentPage"
-                                        :total-rows="rows"
-                                        :per-page="vehicleData.resultsPerPage"
-                                        aria-controls="contactTable"
-                                    ></b-pagination>
-                                </b-row>
-                                
-                            </b-card>
+                                <template #table-busy>
+                                    <div class="text-center my-2">
+                                        <b-spinner style="width: 3rem; height: 3rem;"></b-spinner>
+                                    </div>
+                                </template>
+
+                                <template #cell(actions)="row">
+                                    <b-row align-v="center" align-h="end">
+                                        <b-button @click="openVehicle(row.item)" size="sm" class="btn-icon">
+                                            <b-icon-chevron-right></b-icon-chevron-right>
+                                        </b-button>
+                                    </b-row>
+                                </template>
+
+                            </B-table>
+
+                            <b-row align-h="center" >
+                                <b-pagination
+                                    v-model="vehicleData.currentPage"
+                                    :total-rows="rows"
+                                    :per-page="vehicleData.resultsPerPage"
+                                    aria-controls="contactTable"
+                                ></b-pagination>
+                            </b-row>
+
                         </b-col>
                     </b-row>
                 </b-card>
@@ -102,7 +96,7 @@ export default {
                     tdClass: ''
                 },
             ]
-        }
+        },
     }),
     created() {
         this.vehicleRequest()
@@ -137,8 +131,8 @@ export default {
         
         openVehicle(vehicle) {
             console.log("VEHICLE", vehicle)
-            // this.$store.commit('setSelectedVehicle', vehicle)
-            this.$router.push({path: '/viewVehicle'})
+            this.$store.commit('setSelectedVehicle', vehicle)
+            this.$router.push({path: '/editVehicle'})
         },
     },
     computed: {
