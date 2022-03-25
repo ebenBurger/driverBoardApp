df2efffb4d5a330ebdd9ec7ab957a2ee3ec72a60
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-25 11:09:05 +0200

    router updated paths

--- driverBoardApp/driverBoardApp/src/router/index.js
+++ driverBoardApp/driverBoardApp/src/router/index.js
@@ -4,8 +4,12 @@ import main from "@/views/largeScreen/vehicles/main";
 import createVehicle from "@/views/largeScreen/vehicles/createVehicle";
 import editVehicle from "@/views/largeScreen/vehicles/editVehicle";
 
+//offices
+import officeMain from "@/views/largeScreen/office/officeMain";
+import createOffice from "@/views/largeScreen/office/createOffice";
+
 //contact
-import contacts from "@/views/largeScreen/contacts";
+import contact from "@/views/largeScreen/contact";
 
 export default [
     {
@@ -19,20 +23,33 @@ export default [
                 component: main,
             },
             {
-                path: '/contacts',
-                name: 'contacts',
-                component: contacts,
+                path: '/contact',
+                name: 'contact',
+                component: contact,
             },
             {
-                path: '/addVehicle',
-                name: 'addVehicle',
-                component: createVehicle,
-            },
-            {
-                path: '/editVehicle',
-                name: 'editVehicle',
-                component: editVehicle,
+                path: '/office',
+                name: 'officeMain',
+                component: officeMain,
             },
         ]
-    }
+    },
+    // additional vehicle pages
+    {
+        path: '/addVehicle',
+        name: 'addVehicle',
+        component: createVehicle,
+    },
+    {
+        path: '/editVehicle',
+        name: 'editVehicle',
+        component: editVehicle,
+    },
+    
+    //additional Office pages
+    {
+        path: '/addOffice',
+        name: 'addOffice',
+        component: createOffice,
+    },
 ]  
