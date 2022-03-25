7fac3dc7df95db8a3efeb4eb955dcc040801adcb
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-25 13:27:57 +0200

    contact view and add UI added

--- driverBoardApp/driverBoardApp/src/views/largeScreen/contact/contact.vue
+++ driverBoardApp/driverBoardApp/src/views/largeScreen/contact/contact.vue
@@ -1,12 +1,110 @@
 <template>
     <div>
-        <h1>Contacts</h1>
+        <b-row>
+            <b-col>
+                <b-card>
+                    <b-row>
+                        <b-col>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4 class="m-0">Contact List</h4>
+                            </div>
+                            <b-col class="text-right">
+                                <b-button variant="outline-primary" size="sm" squared @click="addContact">
+                                    <font-awesome-icon icon="fa-plus" class="mr-1"/> Contact
+                                </b-button>
+                            </b-col>
+                        </b-col>
+                    </b-row>
+
+                    <b-row class="mt-3">
+                        <b-col>
+                            <B-table
+                                striped hover
+                                :items="contactTable.dataSource"
+                                :fields="contactTable.tableColumns"
+                                :busy="contactTable.isLoading"
+                                @row-clicked="openContact"
+                                id="vehicleTable"
+                                :current-page="contactTable.currentPage">
+
+                                <template #table-busy>
+                                    <div class="text-center my-2">
+                                        <b-spinner style="width: 3rem; height: 3rem;"></b-spinner>
+                                    </div>
+                                </template>
+
+                                <template #cell(actions)="row">
+                                    <b-row align-v="center" align-h="end">
+                                        <b-button @click="openContact(row.item)" size="sm" class="btn-icon">
+                                            <b-icon-chevron-right></b-icon-chevron-right>
+                                        </b-button>
+                                    </b-row>
+                                </template>
+
+                            </B-table>
+
+                            <b-row align-h="center" >
+                                <b-pagination
+                                    v-model="contactTable.currentPage"
+                                    :total-rows="rows"
+                                    :per-page="contactTable.resultsPerPage"
+                                    aria-controls="contactTable"
+                                ></b-pagination>
+                            </b-row>
+
+                        </b-col>
+                    </b-row>
+                </b-card>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
 export default {
-    data: () => ({}),
+    data: () => ({
+        contact:{
+            name: null,
+            surname: null,
+            email: null,
+            tellNumber: null,
+            mobile: null,
+            company: null,
+            
+        },
+        contactTable: {
+            resultsPerPage: 10,
+            currentPage: 1,
+            dataSource: [],
+            isLoading: false,
+            tableColumns: [
+                {
+                    label: 'Office',
+                    key: 'office',
+                    sortable: true,
+                    tdClass:'',
+                },
+                {
+                    label: 'Address',
+                    key: 'address',
+                    sortable: true,
+                    tdClass:'',
+                },
+                {
+                    label: 'Location',
+                    key: 'location',
+                    sortable: true,
+                    tdClass:'',
+                },
+                {
+                    label: '',
+                    key: 'actions',
+                    sortable: false,
+                    tdClass: ''
+                },
+            ]
+        },
+    }),
     beforeCreate() {
     },
     created() {
@@ -19,8 +117,17 @@ export default {
     },
     updated() {
     },
-    methods: {},
-    computed: {},
+    methods: {
+        addContact() {
+            this.$router.push({path: '/addContact'})
+        },
+        openContact() {},
+    },
+    computed: {
+        rows() {
+            return this.contactTable.dataSource.length
+        },
+    },
 }
 </script>
 
---
+++ driverBoardApp/driverBoardApp/src/views/largeScreen/contact/createContact.vue
@@ -0,0 +1,141 @@
+<template>
+    <div>
+        <b-row class="align-items-center vh-100 m-0 ">
+            <b-col class="d-flex justify-content-center vw-100">
+                <b-card class="w-50">
+                    <b-row>
+                        <B-col>
+                            <h4>Add Contact</h4>
+                        </B-col>
+                    </b-row>
+                    <B-form>
+                        <label class="text-primary font-weight-bold mb-4">Personal Details</label>
+                        <b-row>
+                            <b-col>
+                                <label>Name</label>
+                                <b-form-input v-model="contactData.name"></b-form-input>
+                            </b-col>
+                            <b-col>
+                                <label>Surname</label>
+                                <b-form-input v-model="contactData.surname"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col>
+                                <label>Mobile Number</label>
+                                <b-form-input v-model="contactData.mobile"></b-form-input>
+                            </b-col>
+                            <b-col>
+                                <label>Landline</label>
+                                <b-form-input v-model="contactData.landline"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col cols="6">
+                                <label>Email</label>
+                                <b-form-input v-model="contactData.email"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <hr class="mx-3">
+                        <label class="text-primary font-weight-bold mb-4">Company Details</label>
+                        <b-row>
+                            <b-col>
+                                <label>Building</label>
+                                <b-form-input v-model="contactData.building"></b-form-input>
+                            </b-col>
+                            <b-col>
+                                <label>Office Park</label>
+                                <b-form-input v-model="contactData.officePark"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col>
+                                <label>Address Line 1</label>
+                                <b-form-input v-model="contactData.addressLine1"></b-form-input>
+                            </b-col>
+                            <b-col>
+                                <label>Address Line 2</label>
+                                <b-form-input v-model="contactData.addressLine2"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        
+                        <b-row>
+                            <b-col>
+                                <label>City</label>
+                                <b-form-input v-model="contactData.city"></b-form-input>
+                            </b-col>
+                            <b-col>
+                                <label>Provence</label>
+                                <b-form-input v-model="contactData.provence"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col class="col-2">
+                                <label>Post Code</label>
+                                <b-form-input v-model="contactData.postCode"></b-form-input>
+                            </b-col>
+                        </b-row>
+                        <hr class="mx-3">
+                        <b-row>
+                            <b-col>
+                                <div class="d-flex justify-content-end">
+                                    <div>
+                                        <b-button variant="outline-red" squared @click="goBack" class="ml-2">Cancel</b-button>
+                                    </div>
+                                    <div>
+                                        <b-button variant="primary" squared @click="save" class="ml-2">Save</b-button>
+                                    </div>
+                                </div>
+                            </b-col>
+                        </b-row>
+                    </B-form>
+                </b-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        contactData: {
+            name: null,
+            surname: null,
+            mobile: null,
+            landline: null,
+            email: null,
+            building: null,
+            officePark: null,
+            addressLine1: null,
+            addressLine2: null,
+            postCode: null,
+            city: null,
+            provence: null,
+        },
+    }),
+    beforeCreate() {
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    beforeUpdate() {
+    },
+    updated() {
+    },
+    methods: {
+        goBack() {
+            this.$router.back()
+        },
+        
+        save() {},
+    },
+    computed: {},
+}
+</script>
+
+<style scoped>
+
+</style>
