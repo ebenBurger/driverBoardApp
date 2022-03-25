e3fa8360542b9616ef59ea19c4ac8926161b0938
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-25 13:27:01 +0200

    relationship added to vehicle model

--- driverBoardApp/driverBoard.API/Models/Vehicle.cs
+++ driverBoardApp/driverBoard.API/Models/Vehicle.cs
@@ -4,11 +4,15 @@ namespace driverBoard.API.Models
 {
     public class Vehicle
     {
-        public int Id { get; set; }
+        public int VehicleId { get; set; }
         public string ModelVehicle { get; set; }
         public string MakeVehicle { get; set; }
         public string Location { get; set; }
         public bool? IsActive { get; set; }
         public DateTime DateCreated { get; set; }
+
+        // Relationship setup
+        public int OfficeId { get; set; }
+        public Office Office { get; set; }
     }
 }
