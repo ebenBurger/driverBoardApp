6c1eae5d128626d4093f8c334ba16a5a504438bf
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-03-28 13:03:12 +0200

    store updated with new api implementation

--- driverBoardApp/driverBoardApp/src/store/index.js
+++ driverBoardApp/driverBoardApp/src/store/index.js
@@ -12,9 +12,16 @@ export default new Vuex.Store({
         selectedVehicle: null,
         
         //requests and initial states
+        addressBookRequest: null,
+        addressBookCreateRequest: null,
+        
+        //office
+        officeRequest: null,
+        officeCreateRequest: null,
+        
+        //vehicle
         vehicleRequest: null,
         vehicleCreateRequest: null,
-        // updateVehicleRequest: null,
     },
     
     mutations: {
@@ -23,18 +30,113 @@ export default new Vuex.Store({
             localStorage.setItem('apiUrl', payload)
         },
         
+        //address Book
+        setAddressBookRequest: (state, payload) => {state.addressBookRequest = payload},
+        setAddressBookCreateRequest: (state, payload) => {state.addressBookCreateRequest = payload},
+        
+        //office
+        setOfficeRequest: (state, payload) => {state.officeRequest = payload},
+        setOfficeCreateRequest: (state, payload) => {state.officeCreateRequest = payload},
+        
+        //vehicle
         setVehicleRequest: (state, payload) => {state.vehicleRequest = payload},
         setVehicleCreateRequest: (state, payload) => {state.vehicleCreateRequest = payload},
         setSelectedVehicle: (state, payload) => {state.selectedVehicle = payload},
         
     },
     actions: {
+        //address book
+        getAllAddressBook: ({state}) => {
+          return  new Promise((resolve, reject) => {
+                const callConfig = {
+                    method: 'get',
+                    url: state.baseUrl + 'AddressBook/GetAll',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                axios(callConfig)
+                    .then(response => {
+                        state.addressBookDeatil = response.data
+                        resolve(response)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        createNewAddressBook: ({state}) => {
+            let payload = state.addressBookCreateRequest
+
+            return new Promise((resolve, reject) => {
+                const callConfig = {
+                    method: 'post',
+                    url: state.baseUrl + 'AddressBook/Save',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: payload
+                }
+
+                axios(callConfig)
+                    .then(response => {
+                        resolve(response)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        
+        //Office
+        getAllOffice: ({state}) => {
+            return  new Promise((resolve, reject) => {
+                const callConfig = {
+                    method: 'get',
+                    url: state.baseUrl + 'Office/GetAll',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                axios(callConfig)
+                    .then(response => {
+                        state.OfficeDeatil = response.data
+                        resolve(response)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        createNewOffice: ({state}) => {
+            let payload = state.officeCreateRequest
+
+            return new Promise((resolve, reject) => {
+                const callConfig = {
+                    method: 'post',
+                    url: state.baseUrl + 'Office/Save',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: payload
+                }
+
+                axios(callConfig)
+                    .then(response => {
+                        resolve(response)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        
         //vehicles
         getAllVehiclesRequest: ({state}) => {
             return new Promise((resolve, reject) => {
                 const callConfig = {
                     method: 'get',
-                    url: state.baseUrl + 'Vehicle/getAll',
+                    url: state.baseUrl + 'Vehicle/GetAll',
                     headers: {
                         'Content-Type': 'application/json'
                     }
