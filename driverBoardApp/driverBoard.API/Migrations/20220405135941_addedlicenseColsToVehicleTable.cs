25ac214d93a9e9a994f263224bc4eaaeeb3a6963
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-05 16:42:16 +0200

    migration updated

---
+++ driverBoardApp/driverBoard.API/Migrations/20220405135941_addedlicenseColsToVehicleTable.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace driverBoard.API.Migrations
+{
+    public partial class addedlicenseColsToVehicleTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeA",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeA1",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeB",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeC",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeC1",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeEB",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeEC",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodeEC1",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeA",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeA1",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeB",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeC",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeC1",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeEB",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeEC",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "CodeEC1",
+                table: "Vehicles");
+        }
+    }
+}
--- driverBoardApp/driverBoard.API/Models/Vehicle.cs
+++ driverBoardApp/driverBoard.API/Models/Vehicle.cs
@@ -14,10 +14,18 @@ namespace driverBoard.API.Models
         public int Year { get; set; }
         public bool IsAvailable { get; set; }
         public bool? IsActive { get; set; }
+        public bool CodeA { get; set; }
+        public bool CodeA1 { get; set; }
+        public bool CodeB { get; set; }
+        public bool CodeC1 { get; set; }
+        public bool CodeC { get; set; }
+        public bool CodeEB { get; set; }
+        public bool CodeEC1 { get; set; }
+        public bool CodeEC { get; set; }
         public DateTime DateCreated { get; set; }
         
         public int OfficeId { get; set; }
         public  Office Office { get; set; }
-        
+
     }
 }
