e9e70374fd7c914b7f73b9dccffbfbf576f41bda
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-05 14:23:28 +0200

    address main ui added

---
+++ driverBoardApp/driverBoardApp/src/views/largeScreen/address/addressMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>Address Book</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+    beforeCreate() {
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    beforeUpdate() {
+    },
+    updated() {
+    },
+    methods: {},
+    computed: {},
+}
+</script>
+
+<style scoped>
+
+</style>
