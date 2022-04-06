fee4fea645e970c2c95b51dc4bdffe804d4b699b
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-06 09:01:14 +0200

    added edit contact UI

---
+++ driverBoardApp/driverBoardApp/src/views/largeScreen/contact/editContact.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Edit Contact</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+    beforeCreate() {
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    beforeUpdate() {
+    },
+    updated() {
+    },
+    methods: {},
+    computed: {},
+}
+</script>
+
+<style scoped>
+
+</style>
